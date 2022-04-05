16e777f703e5328e0d486b5440cb2099ec8bcdf7
omarenis <omar.triki@enis.tn>
2022-04-05 02:41:41 +0100

    ready for deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FormComponent} from './screens/form/form.component';
+import {PatientsComponent} from './screens/patients/patients.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PatientsComponent},
+  {path: 'patients/create', component: FormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
